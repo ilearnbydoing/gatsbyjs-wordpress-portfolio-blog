3381b4e661a11a42bf8f7916ed75208fcf3c76a4
Durgesh Gupta <dgweb123@gmail.com>
2020-10-28 09:13:44 +0530

    first commit

---
+++ site-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    siteUrl: "https://durgeshgupta.wordpress.com",
+    postsPerPage: 10,
+    siteTitleMeta: "Durgesh Gupta",
+    siteDescriptionMeta: "A Freelance Full Stack Developer and Marketer",
+    shareImageWidth: 1000,
+    shareImageHeight: 523,
+    shortTitle: "Durgesh Gupta",
+    siteIcon: "favicon.png",
+    backgroundColor: "#e9e9e9",
+    themeColor: "#15171A"
+  };
---
+++ siteConfig.js
@@ -0,0 +1,174 @@
+module.exports ={
+  "siteUrl": "https://my-blog-230a.netlify.app",
+  "postsPerPage": 9,
+  "siteTitleMeta": "My Blog",
+  "siteDescriptionMeta": "My Life My Journey",
+  "shareImageWidth": 1000,
+  "shareImageHeight": 523,
+  "shortTitle": "My Blog",
+  "siteIcon": "favicon.png",
+  "backgroundColor": "#e9e9e9",
+  "themeColor": "#15171A",
+  "apiUrl": "https://wordpress.theasdfghjkl.com",
+  "subscribeWidget": {
+    "visible": true,
+    "title": "Subscribe to My Blog",
+    "helpText": "Get the latest posts delivered right to your inbox.",
+    "successMessage": "Thanks for subscribing to My Blog."
+  },
+  "header": {
+    "navigation": [
+      {
+        "label": "Home",
+        "url": "https://my-blog-230a.netlify.app/"
+      },
+      {
+        "label": "Contact",
+        "url": "https://my-blog-230a.netlify.app/contact"
+      }
+    ]
+  },
+  "footer": {
+    "copyright": "My Blog",
+    "navigation": [
+      {
+        "label": "Home",
+        "url": "https://my-blog-230a.netlify.app/"
+      },
+      {
+        "label": "Sitemap",
+        "url": "https://my-blog-230a.netlify.app/sitemap.xml"
+      },
+      {
+        "label": "RSS",
+        "url": "https://my-blog-230a.netlify.app/rss.xml"
+      },
+      {
+        "label": "Contact",
+        "url": "https://my-blog-230a.netlify.app/contact"
+      }
+    ]
+  },
+  "socialLinks": {
+    "twitter": "",
+    "facebook": "",
+    "instagram": "",
+    "linkedin": "",
+    "github": "",
+    "whatsapp": "",
+    "pinterest": "",
+    "youtube": "",
+    "dribbble": "",
+    "behance": "",
+    "externalLink": ""
+  },
+  "contactWidget": {
+    "title": "Contact My Blog",
+    "successMessage": "We'll get in touch with you soon."
+  },
+  "metadata": {
+    "title": "My Blog",
+    "description": "My Life My Journey"
+  },
+  "twitterCard": {
+    "title": "My Blog",
+    "description": "My Life My Journey",
+    "imageUrl": "",
+    "username": ""
+  },
+  "facebookCard": {
+    "title": "My Blog",
+    "description": "My Life My Journey",
+    "imageUrl": "",
+    "appId": "",
+    "width": 1000,
+    "height": 523
+  },
+  "siteTitle": "My Blog",
+  "siteDescription": "My Life My Journey",
+  "language": "en-US",
+  "logoUrl": "",
+  "iconUrl": "",
+  "coverUrl": "",
+  "alternateLogoUrl": "",
+  "themeConfig": {
+    "variables": [
+      {
+        "varName": "--primary-color",
+        "value": "#2b6cb0"
+      },
+      {
+        "varName": "--primary-color-active",
+        "value": "#2a4365"
+      },
+      {
+        "varName": "--primary-color-light",
+        "value": "#bee3f8"
+      },
+      {
+        "varName": "--sans-font",
+        "value": "Montserrat"
+      },
+      {
+        "varName": "--sans-font-normal",
+        "value": "400"
+      },
+      {
+        "varName": "--sans-font-medium",
+        "value": "500"
+      },
+      {
+        "varName": "--sans-font-semibold",
+        "value": "600"
+      },
+      {
+        "varName": "--sans-font-bold",
+        "value": "700"
+      },
+      {
+        "varName": "--serif-font",
+        "value": "Merriweather"
+      },
+      {
+        "varName": "--serif-font-light",
+        "value": "300"
+      },
+      {
+        "varName": "--serif-font-normal",
+        "value": "400"
+      },
+      {
+        "varName": "--serif-font-medium",
+        "value": "400"
+      },
+      {
+        "varName": "--serif-font-bold",
+        "value": "700"
+      }
+    ],
+    "fonts": [
+      {
+        "family": "Montserrat",
+        "variants": [
+          "400",
+          "500",
+          "600",
+          "700"
+        ],
+        "fontDisplay": "swap",
+        "strategy": "selfHosted"
+      },
+      {
+        "family": "Merriweather",
+        "variants": [
+          "300",
+          "400",
+          "400",
+          "700"
+        ],
+        "fontDisplay": "swap",
+        "strategy": "selfHosted"
+      }
+    ]
+  }
+}
