2a55ef72e24ff64f5fd7d9b6d14cad48d474e586
Durgesh Gupta <dgweb123@gmail.com>
2020-11-06 13:35:11 +0530

    projects update

--- src/@ilearnbydoing/gatsby-wordpress-deva-theme/pages/projects.tsx
+++ src/@ilearnbydoing/gatsby-wordpress-deva-theme/pages/projects.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Header from "../components/Header";
-import FeaturedProjects from "../components/FeaturedProjects";
+import ProjectCard from "../components/ProjectCard";
 
 export default function Projects() {
     const data = useStaticQuery(
@@ -36,7 +36,12 @@ export default function Projects() {
         <Layout>
         <Header headerText="Projects" />
         <section className="container mx-auto">
-          <FeaturedProjects />
+        <div className="flex flex-col items-center justify-center mt-10 mb-10 sm:flex-row">
+              {data.allSitesYaml.edges.map(({ node }, index) => (
+                  <ProjectCard key={`${index}`} projectResolution={node.childScreenshot.screenshotFile.childImageSharp.resolutions} projectTitle={node.title} clientName={node.name} projectDate={node.date} projectExcerpt={node.excerpt} projectUrl={node.url} />
+              ))}
+    
+</div>
             </section>
             </Layout>
     )
