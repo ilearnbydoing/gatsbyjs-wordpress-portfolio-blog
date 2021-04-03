4c14a42ac1e7afbe79899eef3acf85f889dd2b9b
Durgesh Gupta <dgweb123@gmail.com>
2021-04-03 10:02:09 +0530

    whatsapp image link fixed

--- src/@ilearnbydoing/gatsby-wordpress-deva-theme/components/Whatsapp.tsx
+++ src/@ilearnbydoing/gatsby-wordpress-deva-theme/components/Whatsapp.tsx
@@ -13,7 +13,7 @@ export default function Whatsapp() {
     fontSize: `30px`,
     boxShadow: `2px 2px 3px #999`,
       zIndex: `100`}} target="_blank">
-   <img src="images/icons8-whatsapp.svg" alt="Whatsapp" height={32} width={32} style={{ display: `inline`}} />
+   <img src="https://cdn.cdnlogo.com/logos/w/29/whatsapp-icon.svg" alt="Whatsapp" height={32} width={32} style={{ display: `inline`}} />
     </a>
   )
 }
